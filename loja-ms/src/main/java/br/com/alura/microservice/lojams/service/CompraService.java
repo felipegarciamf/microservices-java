14a0e8ac988ff75e1ee85bb153f5d26b507da5be
felipegarciamf <silasfelipegarcia@gmail.com>
2022-04-24 17:50:09 -0300

    criando servidor de governanca de url utilizando eureka server

--- loja-ms/src/main/java/br/com/alura/microservice/lojams/service/CompraService.java
+++ loja-ms/src/main/java/br/com/alura/microservice/lojams/service/CompraService.java
@@ -2,18 +2,22 @@ package br.com.alura.microservice.lojams.service;
 
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import br.com.alura.microservice.lojams.dto.CompraDto;
 import br.com.alura.microservice.lojams.dto.InfoFornecedorDto;
 
+
+@Service
 public class CompraService {
 
+	
 	public void realizaCompra(CompraDto compra) {
 
 		RestTemplate client = new RestTemplate();
 
-		ResponseEntity<InfoFornecedorDto> exchange = client.exchange("http://localhost:8081/info/" + compra.getEndereco().getEstado(), HttpMethod.GET, null,
+		ResponseEntity<InfoFornecedorDto> exchange = client.exchange("http://fornecedor:8081/info/" + compra.getEndereco().getEstado(), HttpMethod.GET, null,
 				InfoFornecedorDto.class);
 
 		
