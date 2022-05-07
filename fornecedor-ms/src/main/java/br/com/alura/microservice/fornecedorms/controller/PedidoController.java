12e52b91234460820a9901597a5505e63ec38c2a
felipegarciamf <silasfelipegarcia@gmail.com>
2022-05-07 10:55:22 -0300

    criando logs da aplicação fornecedor e loja

--- fornecedor-ms/src/main/java/br/com/alura/microservice/fornecedorms/controller/PedidoController.java
+++ fornecedor-ms/src/main/java/br/com/alura/microservice/fornecedorms/controller/PedidoController.java
@@ -2,11 +2,14 @@ package br.com.alura.microservice.fornecedorms.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.alura.microservice.fornecedorms.dto.ItemDoPedidoDTO;
@@ -17,15 +20,19 @@ import br.com.alura.microservice.fornecedorms.service.PedidoService;
 @RequestMapping("pedido")
 public class PedidoController {
 
+	
+	private static final Logger LOG = LoggerFactory.getLogger(PedidoController.class);
+	
 	@Autowired
 	private PedidoService pedidoService;
 	
-	@RequestMapping(method = RequestMethod.POST)
+	@PostMapping
 	public Pedido realizaPedido(@RequestBody List<ItemDoPedidoDTO> produtos) {
+		LOG.info("pedido recebido");
 		return pedidoService.realizaPedido(produtos);
 	}
 	
-	@RequestMapping("/{id}")
+	@GetMapping("/{id}")
 	public Pedido getPedidoPorId(@PathVariable Long id) {
 		return pedidoService.getPedidoPorId(id);
 	}
--- loja-ms/src/main/java/br/com/alura/microservice/lojams/service/CompraService.java
+++ loja-ms/src/main/java/br/com/alura/microservice/lojams/service/CompraService.java
@@ -1,6 +1,12 @@
 package br.com.alura.microservice.lojams.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
@@ -18,23 +24,33 @@ import br.com.alura.microservice.lojams.model.Compra;
 @Service
 public class CompraService {
 
-
+	
+	private static final Logger LOG = LoggerFactory.getLogger(CompraService.class);
+	
 	@Autowired
 	private FornecedorClient fornecedor;
 
-	public Compra realizaCompra(CompraDto compra) {
+	@Autowired
+	private DiscoveryClient discovery;
 
-		InfoFornecedorDto infoPorEstado = fornecedor.getInfoPorEstado(compra.getEndereco().getEstado());
+	public Compra realizaCompra(CompraDto compra) {
 		
+		String estado = compra.getEndereco().getEstado();
+		LOG.info("buscando informações do fornecedor de {}", estado);
+		InfoFornecedorDto infoFornecedor = fornecedor.getInfoPorEstado(estado);
+
+		
+		LOG.info("realizando um pedido");
 		InfoPedidoDto pedido = fornecedor.realizaPedido(compra.getItens());
-		
-		System.out.println(infoPorEstado.getEndereco());
-		
+
+		System.out.println(infoFornecedor.getEndereco());
+
 		Compra compraSalva = new Compra();
-		
+
 		compraSalva.setPedidoId(pedido.getId());
 		compraSalva.setEnderecoDestino(compra.getEndereco().toString());
 		compraSalva.setTempoDePreparo(pedido.getTempoDePreparo());
+
 		
 		return compraSalva;
 
