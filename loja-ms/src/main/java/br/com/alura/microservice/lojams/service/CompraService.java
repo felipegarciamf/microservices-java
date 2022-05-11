2f62bc6f590ed7315e3b63aa90f3f017f61a50a1
felipegarciamf <silasfelipegarcia@gmail.com>
2022-05-10 21:56:21 -0300

    configurando paper trail para log centralizado

--- loja-ms/src/main/java/br/com/alura/microservice/lojams/service/CompraService.java
+++ loja-ms/src/main/java/br/com/alura/microservice/lojams/service/CompraService.java
@@ -1,19 +1,10 @@
 package br.com.alura.microservice.lojams.service;
 
-import java.util.List;
-import java.util.Optional;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
-
-import com.netflix.discovery.converters.Auto;
 
 import br.com.alura.microservice.lojams.client.FornecedorClient;
 import br.com.alura.microservice.lojams.dto.CompraDto;
@@ -30,8 +21,7 @@ public class CompraService {
 	@Autowired
 	private FornecedorClient fornecedor;
 
-	@Autowired
-	private DiscoveryClient discovery;
+
 
 	public Compra realizaCompra(CompraDto compra) {
 		
