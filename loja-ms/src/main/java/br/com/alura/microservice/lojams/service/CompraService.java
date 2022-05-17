1d4e12e24611025d2dc4293539017f432f2fc8ee
felipegarciamf <silasfelipegarcia@gmail.com>
2022-05-16 22:03:18 -0300

    configurando circuit break com hytrix

--- loja-ms/src/main/java/br/com/alura/microservice/lojams/service/CompraService.java
+++ loja-ms/src/main/java/br/com/alura/microservice/lojams/service/CompraService.java
@@ -3,8 +3,9 @@ package br.com.alura.microservice.lojams.service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.stereotype.Service;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 import br.com.alura.microservice.lojams.client.FornecedorClient;
 import br.com.alura.microservice.lojams.dto.CompraDto;
@@ -15,14 +16,13 @@ import br.com.alura.microservice.lojams.model.Compra;
 @Service
 public class CompraService {
 
-	
 	private static final Logger LOG = LoggerFactory.getLogger(CompraService.class);
 	
 	@Autowired
 	private FornecedorClient fornecedor;
 
 
-
+	@HystrixCommand(fallbackMethod = "realizaCompraFallback")
 	public Compra realizaCompra(CompraDto compra) {
 		
 		String estado = compra.getEndereco().getEstado();
@@ -41,9 +41,26 @@ public class CompraService {
 		compraSalva.setEnderecoDestino(compra.getEndereco().toString());
 		compraSalva.setTempoDePreparo(pedido.getTempoDePreparo());
 
+		try {
+			Thread.sleep(20000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		
 		return compraSalva;
 
+	}
+	
+	public Compra realizaCompraFallback(CompraDto compra) {
+		
+	
+		Compra compraFallback = new Compra();
+		
+		compraFallback.setEnderecoDestino(compra.getEndereco().toString());
+		
+		return compraFallback;
 	}
 
 }
