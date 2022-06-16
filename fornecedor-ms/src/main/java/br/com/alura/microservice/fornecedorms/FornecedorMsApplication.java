0d00b4aeb48e208667ab29ed9703b08adc536521
felipegarciamf <silasfelipegarcia@gmail.com>
2022-06-15 22:25:54 -0300

    criando integração de autenticação entre micro serviços

--- fornecedor-ms/src/main/java/br/com/alura/microservice/fornecedorms/FornecedorMsApplication.java
+++ fornecedor-ms/src/main/java/br/com/alura/microservice/fornecedorms/FornecedorMsApplication.java
@@ -2,8 +2,10 @@ package br.com.alura.microservice.fornecedorms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
+@EnableResourceServer
 public class FornecedorMsApplication {
 
 	public static void main(String[] args) {
