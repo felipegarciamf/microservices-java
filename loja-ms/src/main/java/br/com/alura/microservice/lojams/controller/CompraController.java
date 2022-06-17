e7165c74dfaf5a7a283d4e09668ade3c5853055a
felipegarciamf <silasfelipegarcia@gmail.com>
2022-06-16 23:18:28 -0300

    integrando autenticação entre microserviços e zuul

--- loja-ms/src/main/java/br/com/alura/microservice/lojams/controller/CompraController.java
+++ loja-ms/src/main/java/br/com/alura/microservice/lojams/controller/CompraController.java
@@ -1,6 +1,8 @@
 package br.com.alura.microservice.lojams.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -32,6 +34,7 @@ public class CompraController {
 	
 	@PostMapping
 	public Compra realizaCompra(@RequestBody CompraDto compra) {
+		
 		return compraService.realizaCompra(compra);
 	}
 	
